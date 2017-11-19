41c68f1ad25f371e9f4edbd4f628a962e86839c8
jormd <johann.romand@etudiant.univ-reims.fr>
2017-11-19 10:36:43 +0100

    début affichage du dernier article

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine();
+
+        $lastArticle = $em->getRepository('AppBundle:Article')->lastArticlePublish();
+        $articles = $em->getRepository('AppBundle:Article')->articlePublish();
+
+
+        return $this->render('@App/default/list.html.twig', [
+            'lastArticle' => $lastArticle[0],
+            'articles' => $articles
+        ]);
         // replace this example code with whatever you need
-        return $this->render('@App/default/index.html.twig', array(
+       /* return $this->render('@App/default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        ));*/
     }
 }
