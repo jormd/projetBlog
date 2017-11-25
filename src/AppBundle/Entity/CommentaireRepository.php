9a9bad2bf06ed3cffdab2ae06c8ae6e4b10f293e
jormd <johann.romand@etudiant.univ-reims.fr>
2017-11-25 16:16:05 +0100

    correctif requete doctrine

---
+++ src/AppBundle/Entity/CommentaireRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romandjohann
+ * Date: 25/11/2017
+ * Time: 16:13
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class CommentaireRepository extends EntityRepository
+{
+
+}
